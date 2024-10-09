e9f1cad65632a5c54f00f92f73c8b21b098d798d
claddy <0xcladdy@gmail.com>
2024-10-10 01:57:53 +0530

    remove redundant functions
    - use directory-cli to list addresses instead vanilla rpc call
    - use default config values

--- tests/dns.rs
+++ tests/dns.rs
@@ -1,67 +1,21 @@
-use coinswap::{
-    maker::error::MakerError,
-    market::rpc::{RpcMsgReq, RpcMsgResp},
-    utill::{read_message, send_message},
-};
 use std::{
-    fs, io,
-    io::BufRead,
-    net::TcpStream,
     path::PathBuf,
-    process::{Child, Command},
+    process::Command,
     sync::mpsc::{self, Receiver, Sender},
     thread,
     time::Duration,
 };
 
-fn spawn_directoryd_thread(log_sender: Sender<String>) -> Child {
-    let mut directoryd_process = Command::new("cargo")
-        .args(["run", "--bin", "directoryd"])
-        .stdout(std::process::Stdio::piped())
-        .spawn()
-        .expect("Failed to start directoryd process");
-
-    let stdout = directoryd_process.stdout.take().unwrap();
-    thread::spawn(move || {
-        let reader = io::BufReader::new(stdout);
-        for log_message in reader.lines().map_while(Result::ok) {
-            log_sender.send(log_message).unwrap();
-        }
-    });
-    directoryd_process
-}
-
-fn send_rpc_req(req: &RpcMsgReq) -> Result<Option<RpcMsgResp>, MakerError> {
-    let mut stream = TcpStream::connect("127.0.0.1:4321")?;
-    stream.set_read_timeout(Some(Duration::from_secs(20)))?;
-    stream.set_write_timeout(Some(Duration::from_secs(20)))?;
-
-    send_message(&mut stream, req)?;
-
-    let resp_bytes = read_message(&mut stream)?;
-    let resp: RpcMsgResp = serde_cbor::from_slice(&resp_bytes)?;
-    Ok(Some(resp))
-}
-
 #[test]
 fn test_dns() {
     let config_path = PathBuf::from("./.cargo/coinswap-test-data/directory/config.toml");
-    fs::create_dir_all(config_path.parent().unwrap()).unwrap();
-    fs::write(
-        &config_path,
-        "\
-        [directory_config]\n\
-        port = 8084\n\
-        socks_port = 19060\n\
-        connection_type = tor\n\
-        rpc_port = 4321\n\
-        ",
-    )
-    .unwrap();
 
-    let (log_sender, log_receiver): (Sender<String>, Receiver<String>) = mpsc::channel();
+    let (_log_sender, log_receiver): (Sender<String>, Receiver<String>) = mpsc::channel();
 
-    let mut directoryd_process = spawn_directoryd_thread(log_sender);
+    let mut directoryd_process = Command::new("./target/debug/directoryd")
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .unwrap();
 
     let mut server_started = false;
     while let Ok(log_message) = log_receiver.recv_timeout(Duration::from_secs(5)) {
@@ -76,13 +30,12 @@ fn test_dns() {
     );
     thread::sleep(Duration::from_secs(5));
 
-    let resp = send_rpc_req(&RpcMsgReq::ListAddresses).unwrap();
+    let output = Command::new("./target/debug/directory-cli")
+        .args("ListAddresses")
+        .output()
+        .unwrap();
 
-    if let Some(RpcMsgResp::ListAddressesResp(addresses)) = resp {
-        assert!(addresses.is_empty(), "Expected an empty list of addresses");
-    } else {
-        panic!("Unexpected RPC response");
-    }
+    let addresses: Vec<String> = serde_json::from_slice(&output.stdout).unwrap();
 
     directoryd_process
         .kill()
